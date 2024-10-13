2f548532ec6a1acdecbb3ebad40889c877e804a0
codebeautyprogramming <codebeautyprogramming@gmail.com>
2024-10-13 23:39:10 +0200

    Startup code for this chapter

---
+++ CookBook/ConfigurationManager.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookBook
+{
+    public static class ConfigurationManager
+    {
+        public static JObject LoadThemeConfig()
+        {
+            if (File.Exists("themeConfig.json"))
+            {
+                string config = File.ReadAllText("themeConfig.json");
+                return JObject.Parse(config);
+            }
+            return null;
+        }
+    }
+}
